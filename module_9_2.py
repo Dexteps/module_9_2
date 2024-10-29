a617abf109518d0df1f08307075586c715b94f2b
Dexteps <152305884+Dexteps@users.noreply.github.com>
2024-10-29 11:21:58 +0500

    Create module_9_2.py

---
+++ module_9_2.py
@@ -0,0 +1,17 @@
+first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+
+first_res = [len(x) for x in first_strings if len(x) >= 5]
+print(first_res) # Убрать
+
+second_result = [(x, y) for x in first_strings for y in second_strings if len(x) == len(y)]
+
+print(second_result) # убрать
+
+first_strings.extend(second_strings)
+print(first_strings)
+
+third_result = [{x: len(x)} for x in first_strings  if len(x) % 2 == 0 and len(x) % 2 == 0]
+
+
+print(third_result)
